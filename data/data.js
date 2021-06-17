940cb19f6cd2b1bc97ae99b63012c497ef96b1b2
sunzero2 <60477554+sunzero2@users.noreply.github.com>
2021-06-17 18:39:50 +0900

    vue data, template

--- data/data.js
+++ data/data.js
@@ -14,6 +14,7 @@ vm.b = 'hi';
 // b 속성이 나중에 필요한 것을 알고 빈 값이거나 존재하지 않은 상태로 시작해야 한다면
 // 아래와 같이 초기값 설정을 해줘야 함.
 var vm2 = new Vue({
+    el: '#vm2',
     data: {
         a : 1,
         b : ''
@@ -22,6 +23,11 @@ var vm2 = new Vue({
 
 // 얘는 반응함
 vm2.b = 'hi';
+
+vm2.$watch('b', (newValue, oldValue) => {
+    console.log('newValue ', newValue);
+    console.log('oldValue ', oldValue);
+})
 
 var obj = {
     foo: 'bar'
@@ -36,3 +42,34 @@ new Vue({
     data: obj
 })
 
+// 인스턴스 라이프사이클. 자동으로 this 컨텍스트를 인스턴스에 바인딩함
+// created: 인스턴스 생성 시 호출
+// mounted: el -> vm3.$el로 마운트 된 직후 호출 
+// updated: 재렌더링 후 호출. 무한루프가 발생할 수 있으므로 updated에서 상태 변경 X
+// destroyed: 인스턴스 제거 후 호출. 바인딩이 해제, 이벤트 리스너 제거됨
+var vm3 = new Vue({
+    data: {
+        a : 1
+    },
+    created: function () {
+        console.log('a is: ', this.a); // this: vm3 instance
+    },
+    // this: undefined. 화살표 함수는 this를 가지지 않는다.   
+    // created: () => {
+    //     console.log('a is ', this.a); 
+    // }
+    mounted: function () {
+        // 모든 자식 컴포넌트가 마운트 된 상태를 보장
+        this.$nextTick(function () {
+            console.log('모든 화면이 렌더링 된 후 실행');
+        })
+    },
+    updated: function() {
+        this.$nextTick(function () {
+            console.log('전체가 업데이트 된 상태를 보장')
+        })
+    },
+    destroyed: function() {
+        console.log('a is: ', this.a);
+    }
+})
---
+++ template/template.js
@@ -0,0 +1,7 @@
+var vm1 = new Vue({
+    el: '#vm1',
+    data: {
+        msg: '안녕하세요.',
+        rawHtml: '<p style="color: blue">This should be blue.</p>'
+    }
+})
